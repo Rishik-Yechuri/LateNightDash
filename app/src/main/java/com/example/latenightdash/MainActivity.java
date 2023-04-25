075cbdd2e05203c1f08829ae064c1b6aacfaf93c
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-25 12:41:44 -0500

    Stude

--- app/src/main/java/com/example/latenightdash/MainActivity.java
+++ app/src/main/java/com/example/latenightdash/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.latenightdash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    TextToSpeech t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.US);
+                }
+            }
+        });
+        String toSpeak = "SUck my dick bitch, do it quick";
+        Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+        t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
+
+    public void onPause() {
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
 }
